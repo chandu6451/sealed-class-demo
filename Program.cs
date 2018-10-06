26ef33fbf31d38d660ed7a632d8e47b3ad8cab5c
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:50:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClassDemo
+{
+    public sealed class MyClass
+    { }
+
+    public class Calculate : MyClass
+    { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
